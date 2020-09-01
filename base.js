8d41e37ba89eecee0e4583466383893986792d2d
Loyd Schutte <lschutte@sjcourts.org>
2020-09-01 13:36:25 -0700

    Add time.

--- base.js
+++ base.js
@@ -1,3 +1,7 @@
 $(document).ready(function() {
  console.log('Hello world');
+
+    var dt = new Date();
+    var time = dt.getHours() + ":" + dt.getMinutes() + ":" + dt.getSeconds();
+    document.write(time);
 }
